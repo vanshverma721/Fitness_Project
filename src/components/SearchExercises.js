929f774ea20cc09443b0beaf4602e1c47a780e21
vanshverma721 <vvv.721781@gmail.com>
2024-01-02 19:13:57 +0530

    code added

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
 import { exerciseOptions, fetchData } from '../utils/fetchData'
+import HorizontalScrollbar from './HorizontalScrollbar'
 
 const SearchExercises = () => {
 
@@ -80,7 +81,7 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-        <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyParts} bodyPart={bodyParts} />
+        <HorizontalScrollbar data={bodyParts} />
       </Box>
     </Stack>
   )
