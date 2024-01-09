6f973668161b188622db39aabd14834bae409e25
vanshverma721 <vvv.721781@gmail.com>
2024-01-09 23:56:33 +0530

    code added

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,17 +1,25 @@
 export const exerciseOptions = {
-    method: 'GET',
-    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back',
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
-  };
+  method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back',
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+  }
+};
 
 
 export const fetchData = async (url, options) => {
 
-    const res = await fetch(url, options);
-    const data = await res.json();
+  const res = await fetch(url, options);
+  const data = await res.json();
 
-    return data;
+  return data;
 };
