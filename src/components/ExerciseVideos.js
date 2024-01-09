89e2564a53640bfbed67d39e8051eaf45b34d333
vanshverma721 <vvv.721781@gmail.com>
2024-01-10 00:33:21 +0530

    code changed

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -11,7 +11,7 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
         Watch <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>{name}</span> exercise videos
       </Typography>
       <Stack sx={{ flexDirection: { lg: 'row' }, gap: { lg: '110px', xs: '0px' } }} justifyContent="flex-start" flexWrap="wrap" alignItems="center">
-        {exerciseVideos?.slice(0, 3).map((item, index) => (
+        {exerciseVideos?.slice(0, 6).map((item, index) => (
           <a
             key={index}
             className="exercise-video"
@@ -20,6 +20,14 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
             rel="noreferrer"
           >
             <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <Box>
+              <Typography sx={{ fontSize: { lg: '28px', xs: '18px' } }} fontWeight={600} color="#000">
+                {item.video.title}
+              </Typography>
+              <Typography fontSize="14px" color="#000">
+                {item.video.channelName}
+              </Typography>
+            </Box>
           </a>
         ))}
       </Stack>
