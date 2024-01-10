7c90a8e1e105231c30d83d8da451eb106a7df2a1
vanshverma721 <vvv.721781@gmail.com>
2024-01-10 22:16:39 +0530

    changed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,5 @@
 export const exerciseOptions = {
   method: 'GET',
-  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back',
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
