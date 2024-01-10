610df1bd42aabd7ac00d3d453adc020143b96b01
vanshverma721 <vvv.721781@gmail.com>
2024-01-10 22:06:41 +0530

    final changes

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -13,6 +13,12 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
       <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
         {targetMuscleExercises.length !== 0 ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />}
       </Stack>
+      <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px', mt: { lg: '100px', xs: '60px' } }} fontWeight={700} color="#000" mb="33px">
+        Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Equipment</span> exercises
+      </Typography>
+      <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
+        {equipmentExercises.length !== 0 ? <HorizontalScrollbar data={equipmentExercises} /> : <Loader />}
+      </Stack>
     </Box>
   )
 }
