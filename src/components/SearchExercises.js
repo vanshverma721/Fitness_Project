bc9902f59464d6c37ba9e5ddab164a8fc031ac93
vanshverma721 <vvv.721781@gmail.com>
2024-01-13 23:33:08 +0530

    code added

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -80,6 +80,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
       <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
         <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} isBodyParts />
       </Box>
+      <Typography>Hello</Typography>
     </Stack>
   )
 }
